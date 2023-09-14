4054d6555ce4352508376564a6bb1d0685a27e81
stamaki03 <stamaki03@gmail.com>
2023-09-14 11:12:14 +0900

    Initial Commit

---
+++ AozoraBunko/AozoraBunkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AozoraBunkoApp.swift
+//  AozoraBunko
+//
+//  Created by Sho Tamaki on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct AozoraBunkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
