bfcae7e6d844c55e7496e482fbb40ad6a94ca4b1
stamaki03 <stamaki03@gmail.com>
2023-09-17 12:33:33 +0900

    モディファイアの追加

---
+++ AozoraBunko/Components/RoundedButtonWithShadowModifier.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedButtonWithShadowModifier.swift
+//  AozoraBunko
+//
+//  Created by Sho Tamaki on 2023/09/14.
+//
+
+import SwiftUI
+
+struct RoundedButtonWithShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: 80)
+            .foregroundColor(.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.black, lineWidth: 0.5)
+            )
+            .shadow(color: .gray.opacity(0.5), radius: 5, x: 10, y: 10)
+            .padding()
+    }
+}
